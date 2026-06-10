723942660ed534fbd30e7167bdee2e3ad89daceb
Zabi Sideqi <zabi.sideqi@gmail.com>
2026-06-10 17:16:19 +0200

    Add IHandler class

---
+++ GarageExercise/Interface/IHandler.cs
@@ -0,0 +1,14 @@
+﻿using GarageExercise.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageExercise.Interface
+{
+    public interface IHandler
+    {
+        bool AddVehicle(Vehicle vehicle);
+        bool RemoveVehicle(string registrationNumber);
+        Vehicle? FindVehicle(string registrationNumber);
+    }
+}
